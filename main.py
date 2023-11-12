268eaf3d1804ac19d86c9710f9f9663f895ee168
kirannandi896 <150209579+kirannandi896@users.noreply.github.com>
2023-11-12 17:47:48 +0530

    Add files via upload
    Initi

---
+++ main.py
@@ -0,0 +1,35 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+# hello
+class Android_grid(GridLayout):
+    def __init__(self, **kwargs):
+        super(Android_grid, self).__init__()
+        self.rows = 6
+
+        self.add_widget(Label(text="Enter Name:"))
+
+        self.s_name = TextInput()
+        self.add_widget(self.s_name)
+
+        self.add_widget(Label(text="Enter Gender:"))
+        self.s_gender = TextInput()
+        self.add_widget(self.s_gender)
+
+        self.add_widget(Label(text="Enter Age:"))
+        self.s_age = TextInput()
+        self.add_widget(self.s_age)
+
+
+
+class Android_app(App):
+    def build(self):
+
+        return Android_grid()
+    
+
+if __name__ == "__main__":
+    Android_app().run()
